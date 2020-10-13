338925fb536c5c664cac03d113a01884712a2abe
haesleinhuepf <rhaase@mpi-cbg.de>
2020-10-13 16:45:13 +0200

    bugfix: convolve and push are special cases

--- src/main/java/net/haesleinhuepf/clijx/usageminer/UsageMiner.java
+++ src/main/java/net/haesleinhuepf/clijx/usageminer/UsageMiner.java
@@ -1,6 +1,7 @@
 package net.haesleinhuepf.clijx.usageminer;
 
 import ij.IJ;
+import ij.ImageJ;
 import ij.plugin.PlugIn;
 
 import java.io.File;
@@ -12,6 +13,30 @@ public class UsageMiner implements PlugIn {
     @Override
     public void run(String arg) {
         String folder = IJ.getDirectory("Folder with clij-macros");
+        UsageAnalyser combinedUsageStats = new UsageAnalyser(
+                folder);
+
+        String output = combinedUsageStats.all();
+
+
+        String target_file = System.getProperty("java.io.tmpdir") + "/suggestions_" + System.currentTimeMillis() + ".txt";
+
+        File outputTarget = new File(target_file);
+        try {
+            FileWriter writer = new FileWriter(outputTarget);
+            writer.write(output);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        IJ.open(target_file);
+    }
+
+    public static void main(String[] args) {
+        new ImageJ();
+
+
+        String folder = "C:\\Users\\rober\\Downloads\\temp2";
         UsageAnalyser combinedUsageStats = new UsageAnalyser(
                 folder);
 
